61aeea10102f50802b146ca63af50babd1bf00c3
Jeesyuen <25066410@siswa.um.edu.my>
2026-01-08 15:32:25 +0800

    Initial Commit

--- EWN-Puzzle-main/GameLoader.java
+++ EWN-Puzzle-main/GameLoader.java
@@ -1,4 +1,71 @@
+import java.io.*;
+import java.util.Scanner;
 public class GameLoader {
+    private int targetPiece;
+    private int[] initialPosition = new int[6];
+    private int[] diceSequence = new int[30];
+    private int mode;
+
+    public GameLoader(int selectedLevel) {
+        if (selectedLevel < 1 || selectedLevel > 4) {
+            System.out.println("Invalid level");
+        } else {
+            String fileName = "level" + selectedLevel + ".txt";
+
+            try (Scanner input = new Scanner(new FileInputStream(fileName))) {
+                if (input.hasNextInt()) {
+                    targetPiece = input.nextInt();
+
+                    for (int j = 0; j < initialPosition.length; j++) {
+                        initialPosition[j] = input.nextInt();
+                    }
+
+                    for (int k = 0; k < diceSequence.length; k++) {
+                        diceSequence[k] = input.nextInt();
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found: " + fileName);
+            } catch (IOException e) {
+                System.out.println("IO Exception");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printGameDetails(int selectedMode, String humanPlayerName) {
+        this.mode = selectedMode;
+        try {
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream("moves.txt"));
+            if (selectedMode == 0) {
+                outputStream.println("Human Player");
+                outputStream.println("Name: " + humanPlayerName);
+            } else if (selectedMode == 1) {
+                outputStream.println("Random Player");
+            } else if (selectedMode == 2) {
+                outputStream.println("AI Player");
+            } else {
+                outputStream.println("Invalid mode");
+            }
+
+            for (int i = 0; i < diceSequence.length; i++) {
+                outputStream.print(diceSequence[i] + " ");
+            }
+            outputStream.println();
+            outputStream.println(targetPiece);
+
+            for (int j = 0; j < initialPosition.length; j++) {
+                outputStream.print(initialPosition[j] + " ");
+            }
+            outputStream.println();
+            outputStream.close();
+
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+    }
+}
+
 
     // ============================================================
     // TODO: Implement constructor
@@ -19,4 +86,3 @@ public class GameLoader {
 
     // You may also add any other helper functions, variables,
     // and constructors needed for your implementation.
-}
--- EWN-Puzzle-main/GameMain.java
+++ EWN-Puzzle-main/GameMain.java
@@ -1,6 +1,51 @@
+import java.util.Scanner;
 public class GameMain {
+    static int mode;
+    static String name;
+    private static int gameMode() {
+        System.out.println("Enter your game mode (Human = 0, Random = 1, AI = 2): ");
+        Scanner input = new Scanner(System.in);
+        mode = input.nextInt();
+        switch (mode) {
+            case 0:
+                System.out.println("Human Player");
+                name=humanName();
+                break;
+            case 1:
+                System.out.println("Random Player");
+                break;
+            case 2:
+                System.out.println("AI Player");
+                break;
+            default:
+                System.out.println("Invalid Input");
+        }
+        return mode;
+    }
+
+    private static String humanName() {
+        System.out.println("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+        String humanPlayerName = input.nextLine();
+        return humanPlayerName;
+    }
+
+    private static int getLevel() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Select a level (1-4): ");
+        int selectedlevel = input.nextInt();
+        if (selectedlevel >= 1 && selectedlevel<= 4) {
+            GameLoader loader = new GameLoader(selectedlevel);
+            loader.printGameDetails(mode, name);
+        }
+        return selectedlevel;
+    }
 
     public static void main(String[] args) {
+        gameMode();
+        getLevel();
+    }
+}
         // ============================================================
         // TODO: Implement the main() function
         // ------------------------------------------------------------
@@ -29,5 +74,4 @@ public class GameMain {
 
         // You may also add any other helper functions, variables,
         // and constructors needed for your implementation.
-    }
-}
+
