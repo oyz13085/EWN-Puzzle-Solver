ab5d4261aa0cdb36d30599a1df0bbb717ba19947
SI JUNTIAN-2025 <25068553@siswa.um.edu.my>
2026-01-15 15:01:37 +0800

    Add files via upload

---
+++ EWN-Puzzle-main/RandomPlayer.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+import java.util.List;
+
+public class RandomPlayer extends Player{
+    private final Random random = new Random();
+
+    /**
+     * randomly select a legal piece and move position
+     * @param possiblePieces list of movable pieces
+     * @param currentPosition current position array of the piece
+     * @return new position array of the piece after moving
+     */
+
+    @Override
+    public int[] chooseMove(List<Integer> possiblePieces,int[] currentPosition){
+        int[] newPositions = currentPosition.clone();
+
+        if(possiblePieces == null || possiblePieces.isEmpty()){
+            System.out.println("[RandomPlayer]No legal movable pieces,skip this turn.");
+            return newPositions;
+        }
+        int selectedPieceIndex = random.nextInt(possiblePieces.size());
+        int selectedPiece = possiblePieces.get(selectedPieceIndex);
+
+        List<Integer> possibleMovea = GameState.generatePossibleMoves(selectedPiece,newPositions);
+
+        if (possibleMoves == null || possibleMoves.isEmpty()){
+            System.out.println("[RandomPlayer]Piece"+selectedPiece+"No legal moves available,skip this turn.");
+            return newPositions;
+        }
+        int selectedDestIndex = random.nextInt(possibleMoves.size());
+        int selectedDest = possibleMoves.get(selectedDestIndex);
+
+        newPositions[selectedPiece] = selectedDest;
+
+        System.out.println("[RandomPlayer]Execution of move successful:piece"+selectedPiece
+            +"from position"+currentPosition[selectedPiece]
+            +"to position"+selectedDest);
+        
+        return newPositions;
+    }
+}
